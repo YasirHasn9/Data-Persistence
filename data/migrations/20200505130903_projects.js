e9fcdf3f15b95adda0ebba4f98264e898378092f
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-05 13:26:34 -0700

    create resource table

--- data/migrations/20200505130903_projects.js
+++ data/migrations/20200505130903_projects.js
@@ -8,8 +8,18 @@ exports.up = async function(knex) {
     tbl.text("description");
     tbl.boolean("completed").defaultTo("false");
   });
+
+  await knex.schema.createTable("resource", tbl => {
+    tbl.increments("id");
+    tbl
+      .text("name")
+      .notNullable()
+      .unique();
+    tbl.text("description").nullable();
+  });
 };
 
 exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("resource");
   await knex.schema.dropTableIfExists("projects");
 };
